376046ccb3f7554fd5c3f76ae9d36bd650685d31
Thanos Siapas <thanos@caltech.edu>
2024-11-12 09:36:20 -0800

    dj.setup: allow for empty requiredToolboxes

--- +dj/setup.m
+++ +dj/setup.m
@@ -27,48 +27,50 @@ function setup(varargin)
         %     'Version', @(v) compareVersions(v, '2.8.2', @(v_actual,v_ref) v_actual>=v_ref)...
         % )...
     };
-    try
-        ghtb.require(requiredToolboxes, 'prompt', prompt);
-    catch ME
-        installPromptMsg = {
-            'Toolbox ''%s'' did not meet the minimum requirements.'
-            'Would you like to proceed with an upgrade?'
-        };
-        if strcmp(ME.identifier, 'MATLAB:undefinedVarOrClass') && (~prompt || strcmpi('yes',...
-                dj.internal.ask(sprintf(sprintf('%s\n', installPromptMsg{:}), 'GHToolbox'))))
-            % fetch
-            tmp_toolbox = [tempname '.mltbx'];
-            websave(tmp_toolbox, ['https://github.com/' requiredToolboxes{1}.ResolveTarget ...
-                                  '/releases/download/' ...
-                                  subsref(webread(['https://api.github.com/repos/' ...
-                                                   requiredToolboxes{1}.ResolveTarget ...
-                                                   '/releases/latest'], ...
-                                                  weboptions('Timeout', 60)), ...
-                                          substruct('.', 'tag_name')) ...
-                                  '/GHToolbox.mltbx'], weboptions('Timeout', 60));
-            % install
-            try
-                matlab.addons.install(tmp_toolbox, 'overwrite');
-            catch ME
-                if strcmp(ME.identifier, 'MATLAB:undefinedVarOrClass')
-                    matlab.addons.toolbox.installToolbox(tmp_toolbox);
-                else
-                    rethrow(ME);
-                end
-            end
-            % remove temp toolbox file
-            delete(tmp_toolbox);
-            % retrigger dependency validation
+    if ~isempty(requiredToolboxes)
+        try
             ghtb.require(requiredToolboxes, 'prompt', prompt);
-        elseif strcmp(ME.identifier, 'MATLAB:undefinedVarOrClass')
-            GHToolboxMsg = {
-                'Toolbox ''GHToolbox'' did not meet the minimum requirements.'
-                'Please proceed to install it.'
-            };
-            error('DataJoint:verifyGHToolbox:Failed', ...
-                  sprintf('%s\n', GHToolboxMsg{:}));
-        else
-            rethrow(ME)
+        catch ME
+            installPromptMsg = {
+                'Toolbox ''%s'' did not meet the minimum requirements.'
+                'Would you like to proceed with an upgrade?'
+                };
+            if strcmp(ME.identifier, 'MATLAB:undefinedVarOrClass') && (~prompt || strcmpi('yes',...
+                    dj.internal.ask(sprintf(sprintf('%s\n', installPromptMsg{:}), 'GHToolbox'))))
+                % fetch
+                tmp_toolbox = [tempname '.mltbx'];
+                websave(tmp_toolbox, ['https://github.com/' requiredToolboxes{1}.ResolveTarget ...
+                    '/releases/download/' ...
+                    subsref(webread(['https://api.github.com/repos/' ...
+                    requiredToolboxes{1}.ResolveTarget ...
+                    '/releases/latest'], ...
+                    weboptions('Timeout', 60)), ...
+                    substruct('.', 'tag_name')) ...
+                    '/GHToolbox.mltbx'], weboptions('Timeout', 60));
+                % install
+                try
+                    matlab.addons.install(tmp_toolbox, 'overwrite');
+                catch ME
+                    if strcmp(ME.identifier, 'MATLAB:undefinedVarOrClass')
+                        matlab.addons.toolbox.installToolbox(tmp_toolbox);
+                    else
+                        rethrow(ME);
+                    end
+                end
+                % remove temp toolbox file
+                delete(tmp_toolbox);
+                % retrigger dependency validation
+                ghtb.require(requiredToolboxes, 'prompt', prompt);
+            elseif strcmp(ME.identifier, 'MATLAB:undefinedVarOrClass')
+                GHToolboxMsg = {
+                    'Toolbox ''GHToolbox'' did not meet the minimum requirements.'
+                    'Please proceed to install it.'
+                    };
+                error('DataJoint:verifyGHToolbox:Failed', ...
+                    sprintf('%s\n', GHToolboxMsg{:}));
+            else
+                rethrow(ME)
+            end
         end
     end
     % check mym
